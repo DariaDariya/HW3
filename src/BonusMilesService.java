e98ae9fb1d6976517e8b2a7f87c4f41ae88798a5
Daria <dashik_gem@icloud.com>
2023-01-20 23:10:02 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService {
+
+    public int calculate (int price ) {
+        int miles;
+        int amount = 20;
+        miles = price / amount;
+
+        return miles;
+
+    }
+
+
+}
